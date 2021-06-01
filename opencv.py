1212573af19c5ac5f71bf05165a8dbd6dcafb95e
Aysegul Koc <aysegulkoc@Ayseguls-MacBook-Pro.local>
2021-06-01 16:09:01 +0300

    03_01 roi pixel kontorlu

--- opencv.py
+++ opencv.py
@@ -1,28 +1,20 @@
 
-import cv2
-import numpy as np
-
-#img = cv2.imread("deneme2.png")
-cap = cv2.VideoCapture('Untitled.m4v')
-while True :
+from cv2 import cv2
+# img = cv2.imread("deneme2.png")
+cap = cv2.VideoCapture('/Users/aysegulkoc/Movies/3_01.mp4')
+while True:
     _, frame = cap.read()
-    cv2.circle(frame, (767, 1294), 20, (0, 0, 255), -1)
-    cv2.circle(frame, (761, 987), 20, (0, 0, 255), -1)
-    cv2.circle(frame, (1350, 1207), 20, (0, 0, 255), -1)
-    cv2.circle(frame, (340, 1027), 20, (0, 0, 255), -1)
+    (h, w, z) = frame.shape
 
-
-    pts_src = np.array([[0, 0, 0], [74, 0, 0], [0, 40, 0], [-65, 40, 0]])
-    pts_dst = np.array([[767, 1294,], [1350, 1207], [761, 987], [340, 1027]])
-    h, status = cv2.findHomography(pts_src, pts_dst, cv2.RANSAC, 5.0)
-
-    point3D = h * 
-
-
-
-
+    cv2.circle(frame, (639, 600), 20, (0, 0, 255), -1)
+    cv2.circle(frame, (1111, 800), 20, (0, 255, 0), -1)
+    # cv2.circle(frame, (1350, 1207), 20, (0, 0, 255), -1)
+    # cv2.circle(frame, (340, 1027), 20, (0, 0, 255), -1)
     cv2.imshow("Image", frame)
     key = cv2.waitKey(1)
     if key == 27:
         break
 
+print(h)
+print(w)
+print(z)
