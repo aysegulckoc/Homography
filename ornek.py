def7b0c0b3312d3611f19c258b885b68e1a0458d
Aysegul Ceren Koc <aysegul.koc@biges.com>
2021-06-28 15:47:32 +0300

    Deleting old part

--- ornek.py
+++ ornek.py
@@ -1,20 +1,4 @@
-'''
-import numpy as np
-from cv2 import cv2
 
-img_raw = cv2.imread('/Users/aysegulkoc/Pictures/or.png')
-roi = cv2.selectROI(img_raw)
-print(roi)
-
-roi_crop = img_raw[int(roi[1]):int(roi[1]+roi[3]),
-                   int(roi[0]):int(roi[0]+roi[2])]
-
-cv2.imshow("ROI", roi_crop)
-# cv2.imwrite("crop.png",roi_crop )
-
-cv2.waitKey(0)
-
-'''
 from cv2 import cv2
 
 cap = cv2.VideoCapture('/Users/aysegulkoc/Movies/3_01.mp4')
